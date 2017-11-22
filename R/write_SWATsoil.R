9c5087222f87298a0650277bbeb22786cd52cec1
Christoph Schuerz <christoph.schuerz@boku.ac.at>
2017-11-23 00:04:03 +0100

    write_SWAT finished!!!

--- R/write_SWATsoil.R
+++ R/write_SWATsoil.R
@@ -4,13 +4,15 @@
 #' @param sg_cluster
 #' @param n_class
 #'
-#' @importFrom tibble enframe add_column
-#' @importFrom dplyr mutate
+#' @importFrom tibble enframe add_column tibble
+#' @importFrom dplyr mutate select filter group_by summarise_all funs bind_cols
+#' @importFrom purrr map map2 map_df reduce
 #' @importFrom raster writeRaster extent<-
 #' @importMethodsFrom raster extent
 #' @importFrom rgdal writeGDAL
-#' @importFrom magrittr %>%
-#' @importFrom pasta %//%
+#' @importFrom euptf psd2classUS
+#' @importFrom magrittr %>% multiply_by set_names
+#' @importFrom pasta %//% %_%
 #' @import sp
 #'
 #' @return
@@ -52,91 +54,88 @@ write_SWATsoil <- function(project_path, sg_cluster, n_class,
 
   sol_lyr <- sg_cluster$soil_layer
   bdr <-  sol_lyr$bdr %>%
-    add_column(.x, class = clust_sel$value) %>%
-    group_by(.x, class) %>%
-    summarise_all(.x, funs(mean))
+    add_column(., class = clust_sel$value) %>%
+    group_by(., class) %>%
+    summarise_all(., funs(mean))
 
   sol_lyr$bdr <- NULL
 
   z_max <- sg_cluster$layer_meta$depth %>%
     set_names("lyr"%_%1:length(.)) %>%
-    map_df(., function(z, bdr){ifelse(bdr < z, rep(z, length(bdr), bdr))}, bdr) %>%
+    map_df(., ~rep(.x, nrow(bdr))) %>%
+    multiply_by((. < bdr$bdr)) %>%
+      apply(., 1, function(x){
+                    if((sum(x != 0) + 2) <= length(x)){
+                      x[(sum(x != 0) + 2):length(x)] <- NA
+                    }
+                    return(x)}) %>%
+    t(.)
+  z_max <- (z_max + ((!is.na(z_max) & z_max == 0) * bdr$bdr)) %>%
+    as.data.frame(.) %>%
+    as.list(.)
 
 
-
-  sol_lyr <- sg_cluster$soil_layer %>%
+  sol_lyr  %<>%
     # map(., ~ as_tibble(.x)) %>%
     map(., ~ add_column(.x, class = clust_sel$value)) %>%
     map(., ~ group_by(.x, class)) %>%
     map(., ~ summarise_all(.x, funs(mean))) %>%
     map(., function(x){x %>%
              mutate(tex = psd2classUS(snd, slt, cly, orc, option=TRUE))}) %>%
-    map2(., sg_cluster$layer_meta$depth, ~ add_column(.x, z = .y * 10)) %>%
+    map2(., z_max, ~ add_column(.x, z = .y * 10))
+
+  assign_hydgrp <- function(k_s){
+    hyd_grp <- c("D","C","B","A")
+    hyd_trs <- c(3.6,36,144,9999)
+    lapply(k_s, function(x) hyd_grp[hyd_trs > x][1]) %>% unlist()
+  }
 
 
-
-
-
-  return(clust_rst)
-
+arrange_lyr_i <- function(lyr_tbl) {
+  lyr_tbl %>%
+    select(class, z, bld, awc, k_s, orc, cly, slt, snd, crf) %>%
+    mutate(alb    = 0.6/exp(0.4*orc),
+           usle_k = ((0.2 + 0.3*exp(-0.256*snd * (1 - slt/100)))) *
+             ((slt/(cly + slt))^0.3) *
+             (1 - 0.0256*orc / (orc + exp(3.72 - 2.95*orc))) *
+             ((1 - 0.7*(1 - snd/100) /
+                 ((1 - snd/100) + exp(-5.51 + 22.9*(1 - snd/100))))),
+           ec     = 0,
+           k_s = (10^k_s)/2.4)
 }
-#
-# sol_lyr$lyr_0_30    %<>% add_column(z = 300)
-# sol_lyr$lyr_30_100  %<>% add_column(z = 1000)
-# sol_lyr$lyr_100_200 %<>% add_column(z = rst_tbl$zmax %>%
-#                                       add_column(class = clust_14$value) %>%
-#                                       filter(!is.na(class)) %>%
-#                                       group_by(class) %>%
-#                                       summarise_all(funs(mean)) %>%
-#                                       .[[2]] %>%
-#                                       multiply_by(10))
-#
-# assign_hydgrp <- function(k_s){
-#   hyd_grp <- c("D","C","B","A")
-#   hyd_trs <- c(3.6,36,144,9999)
-#   lapply(k_s, function(x) hyd_grp[hyd_trs > x][1]) %>% unlist()
-# }
-#
-# arrange_lyr_i <- function(lyr_tbl) {
-#   lyr_tbl %>%
-#     dplyr::select(z, bld, awc, k_s, orc, cly, slt, snd, crf) %>%
-#     mutate(alb    = 0.6/exp(0.4*orc),
-#            usle_k = ((0.2 + 0.3*exp(-0.256*snd * (1 - slt/100)))) *
-#              ((slt/(cly + slt))^0.3) *
-#              (1 - 0.0256*orc / (orc + exp(3.72 - 2.95*orc))) *
-#              ((1 - 0.7*(1 - snd/100) /
-#                  ((1 - snd/100) + exp(-5.51 + 22.9*(1 - snd/100))))),
-#            ec     = 0,
-#            k_s = (10^k_s)/2.4)
-# }
-#
-# gen <- tibble(objectid  = 1:nrow(sol_lyr[[1]]),
-#               muid      = rep("", nrow(sol_lyr[[1]])),
-#               seqn      = rep("", nrow(sol_lyr[[1]])),
-#               snam      = "solclst"%&%sol_lyr[[1]]$class,
-#               s5id      = rep("", nrow(sol_lyr[[1]])),
-#               cmppct    = rep("", nrow(sol_lyr[[1]])),
-#               nlayers   = length(sol_lyr),
-#               hydgrp    = assign_hydgrp(sol_lyr$lyr_0_30$k_s),
-#               sol_zmx   = sol_lyr$lyr_100_200$z,
-#               anion_exc = 0.5,
-#               sol_crk   = 0.5,
-#               texture   = sol_lyr[[1]]$tex%_%sol_lyr[[2]]$tex%_%
-#                 sol_lyr[[3]]$tex)
-#
-# sol_lyr %<>% lapply(., arrange_lyr_i) %>%
-#   lapply(., round, digits = 2)
-#
-#
-# sol_out <- bind_cols(gen, bind_cols(sol_lyr)) %>%
-#   bind_cols(., data.frame(matrix(0, nrow = nrow(sol_lyr[[1]]),
-#                                  ncol = 152 - ncol(.))))
-#
-# write.table(sol_out, file = wrt_dir%//%"usersoil.csv", quote = FALSE,
-#             row.names = FALSE, col.names = FALSE, sep = ",")
-#
-# sol_lkp <- tibble(VALUE = substr(gen$snam, 9, 11),
-#                   NAME  = gen$snam)
-#
-# write.table(sol_lkp, file = wrt_dir%//%"sol_lkp.csv", quote = FALSE,
-#             row.names = FALSE, col.names = TRUE, sep = ",")
+
+gen <- tibble(objectid  = 1:nrow(sol_lyr[[1]]),
+              muid      = rep("", nrow(sol_lyr[[1]])),
+              seqn      = rep("", nrow(sol_lyr[[1]])),
+              snam      = "soilclass"%&%sol_lyr[[1]]$class,
+              s5id      = rep("", nrow(sol_lyr[[1]])),
+              cmppct    = rep("", nrow(sol_lyr[[1]])),
+              nlayers   = length(sol_lyr),
+              hydgrp    = assign_hydgrp(((10^sol_lyr$lyr_1$k_s)/2.4)),
+              sol_zmx   = round(bdr$bdr, digits = 2),
+              anion_exc = 0.5,
+              sol_crk   = 0.5,
+              texture   = map_dfc(sol_lyr, ~select(.x, tex)) %>%
+                            apply(.,1, paste, collapse = "_"))
+
+sol_lyr %<>%
+  map(.,~ filter(.x,!is.na(z))) %>%
+  map(.,  ~ arrange_lyr_i(.x)) %>%
+  map(., round, digits = 2) %>%
+  reduce(., full_join, by = "class") %>%
+  select(- class)
+
+sol_lyr[is.na(sol_lyr)] <- 0
+
+sol_out <- bind_cols(gen, bind_cols(sol_lyr)) %>%
+  bind_cols(., data.frame(matrix(0, nrow = nrow(.), ncol = 152 - ncol(.))))
+
+write.table(sol_out, file = write_path%//%"usersoil.csv", quote = FALSE,
+            row.names = FALSE, col.names = FALSE, sep = ",")
+
+sol_lkp <- tibble(VALUE = substr(gen$snam, 10, 20),
+                  NAME  = gen$snam)
+
+write.table(sol_lkp, file = write_path%//%"sol_lkp.csv", quote = FALSE,
+            row.names = FALSE, col.names = TRUE, sep = ",")
+}
