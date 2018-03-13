36432c7b38eff30b16946dfb85724dfd5f49dd88
Christoph Schuerz <christoph.schuerz@boku.ac.at>
2018-03-13 17:53:08 +0100

    added function new_soil_project()

---
+++ R/new_soil_project.R
@@ -0,0 +1,25 @@
+#' Initiate a new soil project
+#'
+#' @param project_name the name of the soil project. This will be the name of
+#'   the project folder and the soil project in the working environment. The
+#'   name of this object should not be changed!
+#' @param project_path Path to the SWAT project / Path where soilgrids layers
+#'   are saved
+#' @param shape_file Shape file (or path to shape file) that defines the extent
+#'   of the soilgrids layers.
+
+#' @importFrom pasta %//%
+#'
+#' @return Generates a new soil_project in the working environment (as an R6 object) and generates the prject folder in the project path.
+#' @export
+
+new_soil_project <- function(project_name, project_path, shape_file) {
+  soil_obj <- soil_project$new(project_name = project_name,
+                               project_path = project_path,
+                               shape_file = shape_file)
+
+  assign(project_name, soil_obj, envir = sys.frame(-1))
+  save(list = project_name,
+       file = project_path%//%project_name%//%"soil_project.RData",
+       envir = sys.frame(-1))
+}
--- R/soil_object.R
+++ R/soil_object.R
@@ -16,11 +16,11 @@ soil_project <- R6::R6Class(
   public = list(
     data = list(),
 
-    initialize = function(project_path, shape_file) {
-      if(dir.exists(project_path%//%"soil_layer")){
+    initialize = function(project_name, project_path, shape_file) {
+      if(dir.exists(project_path%//%project_name%//%"soil_layer")){
         stop("Soil project allready exists in"%&&%project_path)
       } else {
-        dir.create(project_path%//%"soil_layer", recursive = TRUE)
+        dir.create(project_path%//%project_name%//%"soil_layer", recursive = TRUE)
       }
       if(is.character(shape_file)){
         shape_file <- readOGR(shape_file, verbose = FALSE)
@@ -28,41 +28,77 @@ soil_project <- R6::R6Class(
         stop("shape_file must be either a shape file of the path to the shape_file!")
         }
 
-      dir.create(project_path%//%"shape_file", recursive = TRUE)
+      dir.create(project_path%//%project_name%//%"shape_file", recursive = TRUE)
       writeOGR(obj = shape_file,
-               dsn = project_path%//%"shape_file"%//%"shp_file.shp",
+               dsn = project_path%//%project_name%//%"shape_file"%//%"shp_file.shp",
                driver = "ESRI Shapefile",
                layer = "shp_file")
 
+      self$data$meta$project_name <- project_name
       self$data$meta$project_path <- project_path
       self$data$shape_file <- shape_file
       self$data$meta$soilgrids$server_path <-
         "http://data.isric.org/geoserver/sg250m/wcs?"
     },
 
-    load_soilgrids = function(soilgrids_server = self$data$meta$soilgrids$server_path){
+    load_soilgrids = function(soilgrids_server = self$data$meta$soilgrids$server_path,
+                              layer_names = c("BDRICM_M_250m",
+                                              "BLDFIE_M_sl"%&%1:7%_%"250m",
+                                              "CLYPPT_M_sl"%&%1:7%_%"250m",
+                                              "CRFVOL_M_sl"%&%1:7%_%"250m",
+                                              "SLTPPT_M_sl"%&%1:7%_%"250m",
+                                              "SNDPPT_M_sl"%&%1:7%_%"250m",
+                                              "CECSOL_M_sl"%&%1:7%_%"250m",
+                                              "ORCDRC_M_sl"%&%1:7%_%"250m",
+                                              "PHIHOX_M_sl"%&%1:7%_%"250m")){
       cat("Downloading soilgrids layer:\n\n")
       layer_meta <- get_layermeta(project_path = self$data$meta$project_path,
-                                  wcs = soilgrids_server )
+                                  wcs = soilgrids_server)
 
       self$data$meta$soilgrids$pixel_size <- layer_meta$pixel_size
       self$data$meta$soilgrids$extent <- layer_meta$extent
-      self$data$meta$soilgrids$layer <-
+      self$data$meta$soilgrids$layer_names <-
         obtain_soilgrids(project_path = self$data$meta$project_path,
                          shp_file = self$data$shape_file,
                          wcs = soilgrids_server,
                          layer_meta = layer_meta,
-                         layer_names = c("BDRICM_M_250m",
-                                         "BLDFIE_M_sl"%&%1:7%_%"250m",
-                                         "CLYPPT_M_sl"%&%1:7%_%"250m",
-                                         "CRFVOL_M_sl"%&%1:7%_%"250m",
-                                         "SLTPPT_M_sl"%&%1:7%_%"250m",
-                                         "SNDPPT_M_sl"%&%1:7%_%"250m",
-                                         "CECSOL_M_sl"%&%1:7%_%"250m",
-                                         "ORCDRC_M_sl"%&%1:7%_%"250m",
-                                         "PHIHOX_M_sl"%&%1:7%_%"250m"))
+                         layer_names = layer_names)
 
       cat("\nLoading soilgrids layer into R:\n\n")
+      soil_data <- load_soilgrids(project_path = self$data$meta$project_path,
+                                  shp_file = self$data$shape_file,
+                                  layer_meta = self$data$meta$soilgrids$layer)
+      private$data_hidden$soil_list <- soil_data$soil_list
+      self$data$raster <- soil_data$soil_raster
+      sel$data$table <- soil_data$soil_table
+
+    },
+
+    test = function(soilgrids_server = self$data$meta$soilgrids$server_path,
+                    layer_names = c("BDRICM_M_250m",
+                                    "BLDFIE_M_sl"%&%1:7%_%"250m",
+                                    "CLYPPT_M_sl"%&%1:7%_%"250m",
+                                    "CRFVOL_M_sl"%&%1:7%_%"250m",
+                                    "SLTPPT_M_sl"%&%1:7%_%"250m",
+                                    "SNDPPT_M_sl"%&%1:7%_%"250m",
+                                    "CECSOL_M_sl"%&%1:7%_%"250m",
+                                    "ORCDRC_M_sl"%&%1:7%_%"250m",
+                                    "PHIHOX_M_sl"%&%1:7%_%"250m")){
+      layer_meta <- get_layermeta(project_path = self$data$meta$project_path,
+                                  wcs = soilgrids_server)
+
+      self$data$meta$soilgrids$pixel_size <- layer_meta$pixel_size
+      self$data$meta$soilgrids$extent <- layer_meta$extent
+      self$data$meta$soilgrids$layer_names <- c("BDRICM_M_250m",
+                                                "BLDFIE_M_sl"%&%1:7%_%"250m",
+                                                "CLYPPT_M_sl"%&%1:7%_%"250m",
+                                                "CRFVOL_M_sl"%&%1:7%_%"250m",
+                                                "SLTPPT_M_sl"%&%1:7%_%"250m",
+                                                "SNDPPT_M_sl"%&%1:7%_%"250m",
+                                                "CECSOL_M_sl"%&%1:7%_%"250m",
+                                                "ORCDRC_M_sl"%&%1:7%_%"250m",
+                                                "PHIHOX_M_sl"%&%1:7%_%"250m")
+
       soil_data <- load_soilgrids(project_path = self$data$meta$project_path,
                                   shp_file = self$data$shape_file,
                                   layer_meta = self$data$meta$soilgrids$layer)
