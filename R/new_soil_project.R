f4f187a8137be7c50a7eac4ff24687808fcc4ba1
Daniel Klotz <daniel.klotz@boku.ac.at>
2018-07-13 15:39:56 +0200

    Updated description for new_soil_project.

--- R/new_soil_project.R
+++ R/new_soil_project.R
@@ -1,17 +1,21 @@
-#' Initiate a new soil project
+#' Initiate a Soil Project
 #'
-#' @param project_name the name of the soil project. This will be the name of
-#'   the project folder and the soil project in the working environment. The
-#'   name of this object should not be changed!
+#' Generates a new soil project (see: \code{\link{soil_project}}), together with
+#'  a fixed link to the project folder and project path
+#' (used to save the data).
+#'
+#' @param project_name The name of the soil project. This will be the name of
+#'   the project folder and the soil project in the working environment.
+#'   Once initialized \strong{the name should not be changed}!
 #' @param project_path Path to the SWAT project / Path where soilgrids layers
-#'   are saved
+#'   are saved. Once initialized \strong{the path should not be changed}!
 #' @param shape_file Shape file (or path to shape file) that defines the extent
-#'   of the soilgrids layers.
-
+#'   of the soilgrids layers. Alternatively, the extend (\code{extend}) and
+#'   coordinate-system (\code{crs}) of a spatial object can be used.
 #' @importFrom pasta %//%
 #'
-#' @return Generates a new soil_project in the working environment (as an R6
-#'   object) and generates the prject folder in the project path.
+#' @return New \code{\link{soil_project}} in the working environment (as an R6
+#'   object), as well as the project folder in the project path.
 #' @export
 
 new_soil_project <- function(project_name, project_path,
