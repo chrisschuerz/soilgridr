2c0419ee5a2b0c28cc121acb02773ef3a4857d64
Daniel Klotz <daniel.klotz@boku.ac.at>
2018-07-13 18:21:51 +0200

    Added descriptions for some functions defined in the R6 obj only

--- R/soil_object.R
+++ R/soil_object.R
@@ -169,3 +169,29 @@ soil_project <- R6::R6Class(
 
   )
 )
+
+#' Start Soil Project from Scratch
+#'
+#' Provides a simple reset for the soil project, by restarting it from scratch.
+#'
+#' @name from_scratch
+NULL
+
+#' Plot clustered soil map
+#'
+#' Plot a clustered soilmap (where the number of clusters is defined by the
+#' \code{\link{select_n_class}} function)
+#'
+#' @name plot_cluster
+NULL
+
+#' Select Number of Clusters
+#'
+#' Select the number of clusters (within kmeans) for further processing.
+#' The function is meant to be used in conjunction with
+#' \code{\link{cluster_soil}} and \code{\link{evaluate_cluster}}.
+#'
+#' @param n Number of clusters.
+#'
+#' @name select_n_class
+NULL
