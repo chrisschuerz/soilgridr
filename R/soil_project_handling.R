1aa2b31015cd6a834f632a657724c4834ed2d0b3
Christoph Schuerz <christoph.schuerz@boku.ac.at>
2018-07-19 11:49:59 +0200

    Changes in the descriptions of sol project handling

--- R/soil_project_handling.R
+++ R/soil_project_handling.R
@@ -1,17 +1,18 @@
 #' Initiate a Soil Project
 #'
 #' Generates a new \strong{soil project} (see: \code{\link{soil_project}}),
-#'   together with a fixed link to the project folder and project path
-#'   (used to save the data).
+#' together with a fixed link to the project folder and project path (used to
+#' save the data).
 #'
 #' @param project_name The name of the soil project. This will be the name of
 #'   the project folder and the soil project in the working environment.
-#'   Once initialized \strong{the name should not be changed}!
-#' @param project_path Path to the SWAT project / Path where soilgrids layers
-#'   are saved. Once initialized \strong{the path should not be changed}!
+#' @param project_path Path where the soil project and its data are saved.
 #' @param shape_file Shape file (or path to shape file) that defines the extent
-#'   of the soilgrids layers. Alternatively, the extend (\code{extend}) and
+#'   of the soilgrids layers. Alternatively, the extent (\code{extent}) and
 #'   coordinate-system (\code{crs}) of a spatial object can be used.
+#' @param extent Extent of the window for which SoilGrids data should be loaded.
+#' @param crs Coordinate system of the provided extent.
+#'
 #' @importFrom pasta %//%
 #'
 #' @return New \code{\link{soil_project}} in the working environment (as an R6
@@ -35,18 +36,11 @@ new_soil_project <- function(project_name, project_path,
 #'   together with a fixed link to the project folder and project path
 #'   (used to save the data).
 #'
-#' @param project_name The name of the soil project. This will be the name of
-#'   the project folder and the soil project in the working environment.
-#'   Once initialized \strong{the name should not be changed}!
-#' @param project_path Path to the SWAT project / Path where soilgrids layers
-#'   are saved. Once initialized \strong{the path should not be changed}!
-#' @param shape_file Shape file (or path to shape file) that defines the extent
-#'   of the soilgrids layers. Alternatively, the extend (\code{extend}) and
-#'   coordinate-system (\code{crs}) of a spatial object can be used.
+#' @param soil_project Character string providing path to the soil project.
 #' @importFrom pasta %//%
 #'
-#' @return New \code{\link{soil_project}} in the working environment (as an R6
-#'   object), as well as the project folder in the project path.
+#' @return The loaded \code{\link{soil_project}} in the working environment (as an R6
+#'   object).
 #' @export
 
 load_soil_project <- function(soil_project) {
