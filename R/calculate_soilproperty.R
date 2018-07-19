9645210fb504776d149c8171d68f15532a6b3582
Christoph Schuerz <christoph.schuerz@boku.ac.at>
2018-07-19 19:26:44 +0200

    implemented function calculate_soilproperty

---
+++ R/calculate_soilproperty.R
@@ -0,0 +1,37 @@
+#' Calculate new soil properties.
+#'
+#' Calculate new soil properties from the \href{https://soilgrids.org/}{soilgrids}}
+#' layers within the \strong{soil project}
+#'
+#' @param soil_list List of tibbles holding soil parameters for the respective
+#'   soil layers.
+#' @param sl Vector defining the soil layers for which the properties are calculated.
+
+#' @importFrom dplyr quo mutate
+#' @importFrom magrittr %<>%
+#' @importFrom purrr map map2 map_at
+#'
+#' @export
+
+
+calculate_soilproperty <- function(soil_list, sl = NULL, ...) {
+
+  fun_list <- as.list(match.call(expand.dots = FALSE))$...
+
+  if(is.numeric(sl)) sl <- "sl"%&%sl
+  layer_avail <- sl %in% names(soil_list)
+  if(!all(layer_avail)) stop("Soil layer given with 'sl' are missing!")
+
+  calc_prop_i <- function(soil_list, sl, var_i, fun_i) {
+    map_at(soil_list, .at = sl, ~ mutate(.x, !! var_i := !! fun_i))
+  }
+
+  new_var <- names(fun_list)
+
+  for(var_i in new_var) {
+    fun_list[[var_i]] <- quo(!!fun_list[[var_i]])
+    soil_list  %<>% calc_prop_i(., sl, var_i, fun_list[[var_i]])
+  }
+
+  return(soil_list)
+}
