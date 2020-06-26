a65b172760bd3621fca7709900cfb405bd9150dd
Christoph Schürz <christoph.schuerz@boku.ac.at>
2020-06-26 12:34:03 +0200

    basic functionality updated (mutate, select, aggregate)

--- R/aggregate_layer.R
+++ R/aggregate_layer.R
@@ -5,73 +5,83 @@
 #'  layers are divided into seven depth-classes (and \code{aggregate_layer}
 #'  function allows to aggregate them over chosen depths.
 #'
-#' @param lower_bound Vector defining the lower bounds of the aggregated soil
+#' @param bottom_depths Vector defining the lower bounds of the aggregated soil
 #'   layers (depths, given in cm).
 
-#' @importFrom magrittr %>% set_names
-#' @importFrom purrr map map2 map_at reduce
+#' @importFrom dplyr filter mutate %>%
+#' @importFrom purrr map map2 reduce set_names
+#' @importFrom tibble tibble
 #' @keywords internal
-aggregate_layer <- function(soil_data, lower_bound) {
+aggregate_layer <- function(soil_data, bottom_depths, depth_table) {
 
   # Checking if all depth for intended aggregation are available:
-  depth_soilgrids <- c(0, 2.5, 10, 22.5, 45, 80, 150)
-  depth_required  <- ("sl"%&%1:7)[depth_soilgrids <= max(lower_bound)]
-  depth_avail <- depth_required %in% names(soil_data)
-  if(!all(depth_avail)) stop("Required soil depths for intended aggregation missing!")
+  max_depth <- max(bottom_depths)
+  if(max_depth > max(depth_table$bottom)) {
+    stop("The maximum 'bottom_depths' is greater than the maximum available depth.")
+  }
 
-  soil_data_aggr <- soil_data[depth_required]
+  depth_table <- filter(depth_table, bottom <= max_depth)
 
-  unique_lyr <- soil_data_aggr %>%
+  if(any(!(depth_table$top[2:nrow(depth_table)] %in% depth_table$bottom))) {
+    stop("A soil depth interval is missing to perform the aggregation over depth!\n",
+         "Solving this issue requires to start a new project and to download all",
+         "soil depth \nintervals that are within the aggregation range.")
+  }
+
+  soil_data <- soil_data[depth_table$sl]
+
+  unique_lyr <- soil_data %>%
     map(., colnames) %>%
     unlist(.) %>%
     unique(.)
 
-  layer_missing <- soil_data_aggr %>%
+  layer_missing <- soil_data %>%
     map(., function(x){!all(unique_lyr %in% colnames(x))}) %>%
     unlist(.) %>%
     any(.)
 
   if(layer_missing) stop("For depth aggregation respective layers must be available for all soil depths!")
 
-  soil_data_aggr %<>% map(., function(x){x[unique_lyr]})
+  soil_data <- map(soil_data, function(x){x[unique_lyr]})
+
 
   # Aggregate soil layers over depth: --------------------------------------------
-  upper_bound <- c(0,lower_bound[1:length(lower_bound) - 1])
+  top_depths <- c(0,bottom_depths[1:length(bottom_depths) - 1])
 
   # Function - calc weights of each layer for respective lower/upper bounds:
-  calc_weights <- function(up_bnd, lw_bnd) {
-    sl_depth <- c(2.5, 7.5, 12.5, 22.5, 35, 70, 50)
-    sl_depth_cum <- cumsum(sl_depth)
-    lw_wgt <- (sl_depth_cum - up_bnd)
-    lw_pos <- which(lw_wgt >= 0)[1]
-    lw_wgt <- (lw_wgt == lw_wgt[lw_pos]) * lw_wgt
-    up_wgt <- lw_bnd - sl_depth_cum
-    up_pos <- which(up_wgt <= 0)[1]
-    up_wgt <- ((up_wgt == up_wgt[up_pos - 1]) * up_wgt)
-    up_wgt <- c(0, up_wgt)[1:7]
-    if((up_pos - 1) >= (lw_pos + 1)){
-      md_wgt <- sl_depth %in% sl_depth[(lw_pos + 1):(up_pos - 1)] * sl_depth
-    } else {
-      md_wgt <- rep(0, 7)
-    }
-    (lw_wgt + md_wgt + up_wgt)/(lw_bnd - up_bnd)
+  calculate_weights <- function(top_dpt, bot_dpt, depth_table) {
+    mutate(depth_table,
+           top = ifelse(top > top_dpt, top, top_dpt),
+           bottom = ifelse(bottom < bot_dpt, bottom, bot_dpt),
+           int = bottom - top,
+           int = ifelse(int >0, int, 0),
+           wgt = int/sum(int)) %>%
+      .$wgt
   }
 
   # Calc the layer weights for each aggregated soil layer
-  layer_weight <- map2(upper_bound, lower_bound, calc_weights) %>%
-    map(., ~.x[1:sum(depth_avail)])
+  layer_weight <- map2(top_depths, bottom_depths, ~ calculate_weights(.x, .y, depth_table))
+
+  depth_table_aggr <- tibble(label = "sl"%&%1:length(top_depths)%&%
+                                    ": "%&%top_depths%-%bottom_depths%&%"cm",
+                            sl = 1:length(top_depths),
+                            top = top_depths,
+                            bottom = bottom_depths
+                            )
 
   # Calc the aggregated soil layers by summing up the weighted layers
-  soil_aggr <- layer_weight %>%
+  soil_data_aggr <- layer_weight %>%
     map(., function(weight, layer_list){
       map2(weight, layer_list, ~.x*.y) %>%
       reduce(`+`)},
-    soil_data_aggr) %>%
+    soil_data) %>%
     map(., as_tibble) %>%
-    set_names("sl"%&%1:length(upper_bound))
+    set_names(depth_table_aggr$label)
+
 
   #Concatenate aggregated soil layer and layers where no aggregation was applied
-  soil_return <- c(soil_aggr, soil_data[!(names(soil_data) %in% ("sl"%&%1:100))])
+  out_list <- list(soil_data = soil_data_aggr,
+                   depth_table = depth_table_aggr)
 
-  return(soil_return)
+  return(out_list)
 }
